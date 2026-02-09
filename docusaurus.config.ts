7b736613d89e0d8cad5a3f280fbc30827ec8065e
候帅 <houshuai0816@126.com>
2026-02-09 14:17:11 +0800

    feat: Add Docusaurus Mermaid theme and enable Mermaid markdown support.

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -134,6 +134,11 @@ const config: Config = {
       darkTheme: prismThemes.dracula,
     },
   } satisfies Preset.ThemeConfig,
+
+  themes: ["@docusaurus/theme-mermaid"],
+  markdown: {
+    mermaid: true,
+  },
 };
 
 export default config;
