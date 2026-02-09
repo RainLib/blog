7abf9e7440cdd4bc26c9ae468de11074c8eb71f1
候帅 <houshuai0816@126.com>
2026-02-09 17:14:00 +0800

    feat: Enable scene selection via URL query parameter, defaulting to all scenes.

--- animation/src/project.ts
+++ animation/src/project.ts
@@ -4,6 +4,21 @@ import http1_flow from "./scenes/http1_flow?scene";
 import http2_flow from "./scenes/http2_flow?scene";
 import quic_flow from "./scenes/quic_flow?scene";
 
+const scenesMap = {
+  http1_flow,
+  http2_flow,
+  quic_flow,
+};
+
+// Parse ?scene=... from the import URL
+const url = new URL(import.meta.url);
+const sceneName = url.searchParams.get("scene");
+
+const selectedScenes =
+  sceneName && sceneName in scenesMap
+    ? [scenesMap[sceneName as keyof typeof scenesMap]]
+    : [http1_flow, http2_flow, quic_flow];
+
 export default makeProject({
-  scenes: [http1_flow, http2_flow, quic_flow],
+  scenes: selectedScenes,
 });
