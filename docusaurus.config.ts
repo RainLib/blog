1f76a75fbc069b1c8308bad77c7caa9535c5b623
候帅 <houshuai0816@126.com>
2026-03-07 15:27:39 +0800

    chore: Update GitHub navigation link to point to the blog repository.

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -131,7 +131,7 @@ const config: Config = {
         { to: "/blog", label: "Blog", position: "left" },
         { to: "/recommend", label: "Recommend", position: "left" },
         {
-          href: "https://github.com/RainLib",
+          href: "https://github.com/RainLib/blog",
           label: "GitHub",
           position: "right",
         },
